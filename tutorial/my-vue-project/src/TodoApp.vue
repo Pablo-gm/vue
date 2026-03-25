d4c55219a589f796b6d3bf8dc10dbd52ff403120
Pablo Gomez <pablogomez321@gmail.com>
2026-03-25 10:23:40 -0600

    adding edit functionality

--- tutorial/my-vue-project/src/TodoApp.vue
+++ tutorial/my-vue-project/src/TodoApp.vue
@@ -1,14 +1,25 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, computed, watch, onMounted } from 'vue'
 const newTask = ref('')
 const tasks = ref([])
 
 const editingId = ref(null)
 const editingBuffer = ref("")
 
-function addTask(){
+const search = ref('');
+const filters = ['All', 'Completed', 'Incompleted', 'Favorites'];
+const activeFilter = ref('All');
+
+onMounted(() => {
+    const saved = localStorage.getItem('tasks');
+    if(saved){
+        tasks.value = JSON.parse(saved);
+    }
+})
+
+function addTask() {
     const text = newTask.value.trim();
-    if(!text){
+    if (!text) {
         return
     }
 
@@ -23,14 +34,60 @@ function addTask(){
     console.log(tasks);
 }
 
-function removeTask(idToRemove){
-    tasks.value = tasks.value.filter(( t => t.id !== idToRemove))
+function removeTask(idToRemove) {
+    tasks.value = tasks.value.filter((t => t.id !== idToRemove))
 }
 
-function startEdit(task){
+function startEdit(task) {
     editingId.value = task.id;
-    editingBuffer.value = task.text
+    editingBuffer.value = task.text;
 }
+
+function cancelEdit() {
+    editingId.value = null;
+    editingBuffer.value = "";
+}
+
+function finishEdit(task) {
+    if (editingId.value !== task.id) {
+        return;
+    }
+
+    const trimmed = editingBuffer.value.trim();
+
+    if (!trimmed) {
+        removeTask(task.id);
+    } else {
+        task.text = trimmed;
+    }
+
+    cancelEdit();
+}
+
+function toggleFav(task) {
+    task.favorite = !task.favorite;
+}
+
+const filteredTasks = computed(() => {
+    return tasks.value
+        .filter(tsk => tsk.text.toLowerCase().includes(search.value.toLowerCase()))
+        .filter(tsk => {
+            if(activeFilter.value === 'Completed') return tsk.completed;
+            if(activeFilter.value === 'Incompleted') return !tsk.completed;
+            if(activeFilter.value === 'Favorites') return tsk.favorite;
+            return true;
+    });
+});
+
+watch(
+    tasks, 
+    () => {
+        localStorage.setItem('tasks', JSON.stringify(tasks.value));
+    },
+    { deep: true},
+)
+
+
 </script>
 
 <template>
@@ -50,18 +107,47 @@ function startEdit(task){
                     </div>
                 </div>
 
+                <div class="mb-3">
+                    <label for="searchInput" class="form-label">Search</label>
+                    <input type="text" class="form-control" id="searchInput" aria-describedby="searchHelp"
+                        v-model="search">
+                    <div id="searchHelp" class="form-text">Something</div>
+                </div>
+
+                <div class="btn-group mb-4" role="group" aria-label="Filter buttons">
+                    <button @click="activeFilter = fil" v-for="fil in filters" :key="fil" class="btn btn-outline-dark"
+                        :class="{ 'active': activeFilter === fil }">{{ fil }}</button>
+                </div>
+
                 <ul class="list-group">
-                    <li v-for="task in tasks" class="list-group-item d-flex align-items-center justify-content-between" :key="task.id">
-                        <span>
-                            <input class="form-check-input me-1" type="checkbox" v-model="task.completed" :id="task.id">
-                            <label class="form-check-label ms-2" :class="{'text-decoration-line-through': task.completed}"  :for="task.id">{{task.text}}</label>
-                        </span>
-                        <span class="float-end">
-                            <button type="button" class="btn btn-danger" @click="removeTask(task.id)">X</button>
-                            <button type="button" class="btn btn-info" @click="editTask(task.id)">Edit</button>
-                            <button type="button" class="btn btn-warning ms-2">Fav</button>
-                        </span>
-                        
+                    <li v-for="task in tasks" class="list-group-item d-flex align-items-center justify-content-between"
+                        :key="task.id">
+                        <template v-if="editingId === task.id">
+                            <input type="text" class="form-control" v-model="editingBuffer"
+                                @keyup.enter="finishEdit(task)" :ref="el => el && el.focus()" />
+                            <span class="float-end text-nowrap">
+                                <button type="button" class="btn btn-success ms-2"
+                                    @click="finishEdit(task)">Done</button>
+                                <button type="button" class="btn btn-danger ms-2" @click="cancelEdit()">Cancel</button>
+                            </span>
+                        </template>
+                        <template v-else>
+                            <span>
+                                <input class="form-check-input me-1" type="checkbox" v-model="task.completed"
+                                    :id="task.id">
+                                <label class="form-check-label ms-2"
+                                    :class="{ 'text-decoration-line-through': task.completed }"
+                                    :for="task.id">{{ task.text }}</label>
+                                <span v-if="task.favorite" class="badge text-bg-warning ms-1">Favorite</span>
+                            </span>
+                            <span class="float-end text-nowrap">
+                                <button type="button" class="btn btn-danger" @click="removeTask(task.id)">X</button>
+                                <button type="button" class="btn btn-info ms-2" @click="startEdit(task)">Edit</button>
+                                <button type="button" class="btn btn-warning ms-2"
+                                    :class="[task.favorite ? 'btn-light' : 'btn-warning']" @click="toggleFav(task)">{{
+                                    task.favorite ? 'Unfav' : 'Fav' }}</button>
+                            </span>
+                        </template>
                     </li>
                 </ul>
             </div>
